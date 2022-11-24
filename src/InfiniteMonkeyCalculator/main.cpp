7af1d6f440319534c0043ada0c4eab6832885963
o7q <jev.o7q@gmail.com>
2022-11-24 13:23:28 -0600

    Minor Release [v1.1.0]
    - Display attempts while running
    - Using printf instead of cout to make displaying progressing faster
    - Made max attempts cap at the 64-bit integer instead of the 32-bit one (like you were even going to get to the 32-bit integer anyways :P)

--- src/InfiniteMonkeyCalculator/main.cpp
+++ src/InfiniteMonkeyCalculator/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void cc();
 
-const string ver = "v1.0.0";
+const string ver = "v1.0.1";
 
 main()
 {
@@ -17,17 +17,16 @@ main()
     {
         cout << "\n+========================================+\n";
 
-        cout << "\nSEARCH PHRASE (lowercase only):\n- ";
-        string input;
-        cin >> input; cc();
+        cout << "\nSEARCH PHRASE (lowercase only, no spaces):\n- ";
+        string input; cin >> input; cc();
 
-        cout << "\nMAXIMUM ATTEMPTS:\n- ";
-        int attempts;
-        cin >> attempts; cc();
+        cout << "\nMAXIMUM ATTEMPTS (! for infinite):\n- ";
+        string attempts_str; cin >> attempts_str; cc();
+        long long int attempts;
+        attempts = attempts_str == "!" ? 9223372036854775807 : stoi(attempts_str);
 
         cout << "\nDISPLAY OUTPUT? (slow) (y = yes | anything else = no)\n- ";
-        char displayOutput;
-        cin >> displayOutput; cc();
+        char displayOutput; cin >> displayOutput; cc();
         cout << endl;
 
         const int input_length = input.length();
@@ -39,22 +38,21 @@ main()
 
         srand(time(NULL));
         mt19937 gen(rand());
-        uniform_int_distribution<> distr(0, 25);
+        uniform_int_distribution<> dist(0, 25);
 
         for (int i = 0; i < attempts; i++)
         {
             string str;
-            for (int n = 0; n < input_length; n++) { char c = 'a' + distr(gen); str += c; }
+            for (int n = 0; n < input_length; n++) { char c = 'a' + dist(gen); str += c; }
             count++;
 
-            if (displayOutput == 'y') cout << str << endl;
+            if (displayOutput == 'y') printf((str + " | " + to_string(count) + "\n").c_str());
 
             if (str == input)
             {
                 isRunning = false;
                 cout << "\nFOUND \"" + input + "\" IN " + to_string(count) + " " + attemptsText + "! (max: " + to_string(attempts) + ")\nTRY ANOTHER? (y = yes | anything else = no)\n- ";
-                char r;
-                cin >> r; cc();
+                char r; cin >> r; cc();
                 if (r == 'y') break; else return 0;
             }
             finalStr = str;
@@ -63,8 +61,7 @@ main()
         if (finalStr != input && isRunning)
         {
             cout << "\nCOULD NOT FIND \"" + input + "\" IN " + to_string(attempts) + " " + attemptsText +"!\nRETRY? (y = yes | anything else = no)\n- ";
-            char r;
-            cin >> r; cc();
+            char r; cin >> r; cc();
             if (r != 'y') return 0;
         }
     }
